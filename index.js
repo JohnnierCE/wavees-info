4b0c4a65ccce46fbce64f673b2da701e3a0975e6
JohnnierCE <114421611+JohnnierCE@users.noreply.github.com>
2025-08-14 20:25:48 -0500

    index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const TELEGRAM_TOKEN = "8308992460:AAHoSoA9rWhHJCt9FuX2RkdBCVhmdnSX6d8";
 const CHAT_ID = "5703312558";
 
 const GT_API = {
-  nombre: "GT",
-  url: "https://gt.integra-metrics.com/api/v2/estado-soft?data=%7B%7D",
-  token: "1b9b06749016cd1c3bfe0ce075ef1682745c04cfb81268370ac129da1ae4052a8635fd4b323e35b317b360a64d7e117f52cea4f5386b760f3d9c847e43357abd"
+  nombre: "RD",
+  url: "https://rd.integra-metrics.com/api/v2/estado-soft?data=%7B%7D",
+  token: "7c0a5d5e456db8b238879426b52d504ecd087a98c574d69d63ffb3868cf6f9b8c30ff4fbda7a265c91e70769b90497c07335cb02d0af8eca7f94a724103aaa80"
 };
 
 // Escapar caracteres especiales para MarkdownV2
