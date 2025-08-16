8dc4bec9261956810565ef7694ca9d0ed82d940b
JohnnierCE <114421611+JohnnierCE@users.noreply.github.com>
2025-08-15 19:28:28 -0500

    index.js

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ async function consultarPais(pais) {
 const alertas = Object.values(latestPerPc).map(pc => {
     const alertasPC = [];
     const primarioLibre = toGB(pc.primary_disk_total - pc.primary_disk_used);
-    if (primarioLibre < 10) alertasPC.push(`FEED ${pc.id_pc} 💽 - Primario ALERTA (${primarioLibre}/${toGB(pc.primary_disk_total)} GB)`);
+    if (primarioLibre < 10) alertasPC.push(`FEED ${pc.id_pc} Disco primario en Alerta (${primarioLibre} de ${toGB(pc.primary_disk_total)} GB)`);
 
     const secundarioLibre = toGB(pc.secondary_disk_total - pc.secondary_disk_used);
-    if (secundarioLibre < 5) alertasPC.push(`FEED ${pc.id_pc} 💽 - Secundario ALERTA (${secundarioLibre}/${toGB(pc.secondary_disk_total)} GB)`);
+    if (secundarioLibre < 5) alertasPC.push(`FEED ${pc.id_pc} Disco secundario en Alerta (${secundarioLibre} de ${toGB(pc.secondary_disk_total)} GB)`);
 
     // Si hay alertas, poner el ⚠️ arriba y abajo
     if (alertasPC.length > 0) {
